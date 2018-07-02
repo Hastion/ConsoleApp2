e9a08c86688e6d7458d932daec5e33a6ad16eed6
Radoslaw <rbinka@gmail.com>
2018-07-02 18:52:57 +0200

    Hello user

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,7 +10,10 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            string name = "user";
+            Console.Write("Enter your name: ");
+            name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}");
         }
     }
 }
